34da3450e742af51bbd0d429a6d0b5d57a9b3aa2
Marynamore <marinaoliverbri674@gmail.com>
2023-06-29 10:15:36 -0300

    Falta adicionar mais info

--- conceitos_basicos/array_multidimensional/index.php
+++ conceitos_basicos/array_multidimensional/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Array Multidimensional<</title>
 </head>
-<body style="background-color:#cccccc">
+<body>
     <h1 style="color:darkblue"; align="center">!! A funcao do array multidimensional !! </h1>
     <hr>
     <center>
